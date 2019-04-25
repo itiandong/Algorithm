0aa7c679122b58d55c53c645d487fd9ec00e12c9
itiandong <j@itiandong.com>
2019-04-26 00:16:32 +0800

    add QuickSort

---
+++ src/QuickSort.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+public class QuickSort {
+    static final Random rand = new Random();
+
+    public static void main(String[] args) {
+        int[] testArr = new int[20];
+
+        int n=5;
+        while (n-- > 0) {
+            for (int i = 0; i < 20; ++i) {
+                testArr[i] = rand.nextInt(100);
+            }
+            System.out.println("BeforeSort:");
+            for(int e: testArr) System.out.print(e + " ");
+            System.out.println();
+            quickSort(testArr);
+            System.out.println("AfterSort:");
+            for(int e: testArr) System.out.print(e + " ");
+            System.out.println();
+        }
+
+    }
+
+    static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length);
+    }
+
+    /**
+     * 对 [lo, hi) 排序。
+     */
+    static void quickSort(int[] arr, int lo, int hi) {
+        if(hi -lo < 2) return; //单区间自然有序
+
+        int mi = partition(arr, lo, hi-1); //轴点已经排序
+        quickSort(arr, lo, mi);
+        quickSort(arr, mi + 1, hi);
+    }
+
+    /**
+     * 在 [lo, hi] 中寻找轴点（pivot）
+     */
+    private static int partition(int[] arr, int lo, int hi) {
+        swap(arr, lo, rand.nextInt(hi-lo+1));
+        int pivot = arr[lo];
+        while (lo < hi) {
+            while ((lo < hi) && pivot <= arr[hi]) {
+                hi--;
+            }
+            arr[lo] = arr[hi];
+            while ((lo < hi) && arr[lo] <= pivot) {
+                lo++;
+            }
+            arr[hi] = arr[lo];
+        }
+        assert lo==hi;
+        arr[lo] = pivot;
+        return lo;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        if(a == b) return;
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
